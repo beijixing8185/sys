7e723a25f3ed395e39fc012694f92046e2c1a5fd
zf <574481793@qq.com>
2018-10-19 17:39:10 +0800

    修改路由

--- routes/api.php
+++ routes/api.php
@@ -37,7 +37,7 @@ $api->version('v1',function($api){
 
     //测试
     $api->group(['namespace' => 'App\Controllers\V1','middleware'=>'jwt.auth'], function ($api) { //
-        $api->get('test','TestController@test'); //临时测试
+        $api->post('test','TestController@test'); //临时测试
     });
 
 
